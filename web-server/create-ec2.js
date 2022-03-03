4ee866b60b93abdd0e2f76bb01e4af48ea3e77c5
Susant Kumar Palai <palai.susant@gmail.com>
2022-03-03 13:53:30 -0700

    Scale up
    Signed-off-by: Susant Kumar Palai <palai.susant@gmail.com>

--- web-server/create-ec2.js
+++ web-server/create-ec2.js
@@ -1,5 +1,7 @@
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
+const { getConfig } = require('../config/config-reader');
+const { addInstanceIdToMap, getCurrentInstanceCount } = require('./instanceMap');
 // Load credentials and set region from JSON file
 AWS.config.update({region: 'us-east-1'});
 
@@ -13,53 +15,36 @@ var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
 
 //return 0 : success
 //      -1 : failure
-function ec2_create() {
-    var status = 0;
-
+function ec2_create(nameId) {
     // define AMI
     var instanceParams = {
-        ImageId: 'ami-0fdc2ea03ba54acd6',  //get this from configuration file
+        ImageId: getConfig().AMI,  //get this from configuration file
         InstanceType: 't2.micro',
         MinCount: 1,
         MaxCount: 1
     };
 
-    // Create a promise on an EC2 service object
-    var instancePromise = new AWS.EC2({apiVersion: '2016-11-15'}).runInstances(instanceParams).promise();
+    return new Promise ((resolve, reject) => {
+        // Create a promise on an EC2 service object
+        var instancePromise = new AWS.EC2({apiVersion: '2016-11-15'}).runInstances(instanceParams).promise();
 
-    // Handle promise's fulfilled/rejected states
-    instancePromise.then(
-        function(data) {
+        // Handle promise's fulfilled/rejected states
+        instancePromise.then( data => {
             console.log(data);
             var instanceId = data.Instances[0].InstanceId;
             console.log("Created instance", instanceId);
-            // Add tags to the instance
-            tagParams = {Resources: [instanceId], Tags: [
-            {
-                Key: 'Name',
-                Value: 'InstanceCreated' // need a unique name
-            }
-            ]};
-        
-            // Create a promise on an EC2 service object
-            var tagPromise = new AWS.EC2({apiVersion: '2016-11-15'}).createTags(tagParams).promise();
-            // Handle promise's fulfilled/rejected states
-            tagPromise.then(
-                function(data) {
-                console.log("Instance tagged");
-            }).catch(
-                function(err) {
-                console.error(err, err.stack);
-                return -1;
-            });
-    }).catch(
-        function(err) {
-        console.error(err, err.stack);
-        return -1;
-    });
-    
-    
-    return status;
+            addInstanceIdToMap(instanceId, data)
+            console.log("Current instance count " + getCurrentInstanceCount());
+            resolve()
+        }).catch( err => {
+            console.error(err, err.stack);
+            reject();
+        });
+    })
+
+  
 }
+
+ec2_create()
 
 module.exports = {ec2_create};
