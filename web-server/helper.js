c75080e3c0a7431baadaa7773b848666a0bbca99
Susant Kumar Palai <palai.susant@gmail.com>
2022-03-09 13:17:00 -0700

    encoding change
    Signed-off-by: Susant Kumar Palai <palai.susant@gmail.com>

--- web-server/helper.js
+++ web-server/helper.js
@@ -1,10 +1,11 @@
+
 var fs = require('fs');
 const sqs_utility = require('./sqs-utility.js');
 
 /* encode image */
-function base64_encode(file, type) {
-    console.log(file)
-    return "data:image/"+type+";base64,"+fs.readFileSync(file, 'base64');
+function base64_encode(file, imageName, hostname) {
+    // console.log(file)
+    return imageName+","+fs.readFileSync(file, 'base64')+","+hostname;
 }
 
 async function processResponseQ() {
