621b0ad799856121820437f9768de55370ad2ac8
Eshita Khandelwal <eshitaaraj@gmail.com>
2022-03-09 13:08:05 -0700

    'changes'

--- app-server/app.py
+++ app-server/app.py
@@ -1,6 +1,7 @@
 from cProfile import run
 from urllib import request, response
 import boto3, json
+from botocore.exceptions import NoCredentialsError
 import subprocess
 import requests
 import base64
@@ -64,6 +65,32 @@ def ping_webserver():
 def save_to_s3():
     return
 
+def upload_to_aws(local_file, bucket, s3_file):
+    s3 = boto3.client('s3')
+
+    try:
+        s3.upload_file(local_file, bucket, s3_file)
+        print("Upload Successful")
+        return True
+    except FileNotFoundError:
+        print("The file was not found")
+        return False
+    except NoCredentialsError:
+        print("Credentials not available")
+        return False
+
+def upload_result(bucket_name,file_name,txt_data):
+    session=boto3.Session(profile_name='default')
+
+    s3 = session.resource('s3')
+
+    object = s3.Object(bucket_name, file_name)
+
+    result = object.put(Body=txt_data)
+
+    res = result.get('ResponseMetadata')
+
+
 if __name__ == "__main__":
     loop_count = 0
     while(loop_count < 6):
@@ -78,6 +105,9 @@ if __name__ == "__main__":
             output = output[:len(output)-1]
             
             send_message(file=image_name[:len(image_name) - 4], output=output)
+            uploaded = upload_to_aws(r'C:\Users\Dell\Desktop\cloud computing\face_images_100\test_00.jpg',
+                                     'ccinputimages', 'Test_00')
+            upload_result1 = upload_result('recognitionresults', 'test_00', 'Paul')
         
         else:
             loop_count += 1
